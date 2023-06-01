956cc18ca966837bbfd8afbc54211d416dc548a5
Nikita Allaglo <allaglo@lisn.fr>
2023-06-01 13:29:20 +0200

    Added option to delay calculations to duplicate and averages

--- source/calculations.py
+++ source/calculations.py
@@ -21,12 +21,14 @@ class calculations(database):
 
         self.base_chunk = self.db['u']['field'].chunksize
 
-    def norm(self, key):
+    def norm(self, key, delayed=True):
         # computes the norm of a field present in the db
         self.key_check(key)
-        return da.linalg.norm(self.db[key]['field'], ord=2, axis=0, keepdims=True)
+        mod = self.general_module(delayed)
+
+        return mod.linalg.norm(self.db[key]['field'], ord=2, axis=0, keepdims=True)
     
-    def mean(self, field, type):
+    def mean(self, field, type, delayed=True):
         '''
         Computing of a mean quantity along a certain axis depending on the type of averaging.
         field must have the same structure as typical objects of the db (ie self.dims).
@@ -39,37 +41,39 @@ class calculations(database):
         the mean for each computation ...
         '''
         self.mean_type_check(type)
- 
+        mod = self.general_module(delayed)
+
         s = 1           # T axis
         w = None    
         if type != 'temporal':
             s = (2,3)   # P and N axis
-            w = self.duplicate(da.from_array(self.db['v_weight']), field)
-        avg = da.average(field, axis=s, weights=w)
+            w = self.duplicate(self.db['v_weight'], field, delayed)
+        avg = mod.average(field, axis=s, weights=w)
         if type != 'both':
             return avg
         else:
             # if its 'both', you need to temporally average the spatial average
-            return da.average(avg, axis=None, weights=None)
+            return mod.average(avg, axis=None, weights=None)
     
-    def moment(self, field, type, n, raw=True):
+    def moment(self, field, type, n, raw=True, delayed=True):
+        '''
+        field is already computed in case the delayed option is set to true
+        '''
         # same principle as in mean function
         self.mean_type_check(type)
 
         if raw:
             avg = 0
         else:
-            avg = self.mean(field, type)
-            avg = self.duplicate(avg, field)
-
+            avg = self.mean(field, type, delayed)
+            avg = self.duplicate(avg, field, delayed)
         to_compute = (field - avg)**n
 
-        return self.mean(to_compute, type)
+        return self.mean(to_compute, type, delayed)
 
     '''
-    For the pdfs, fields must be scalar ones having SAME shape.
+    For the pdfs, fields must be scalar ones.
     TO-DO:
-    - Study the importance of the choice of bins
     - Rechunk more cleverly
     '''
     def pdf(self, field, bins=1000, range=None, delayed=True):
@@ -77,13 +81,12 @@ class calculations(database):
         If not delayed, fields must already be computed!
         '''
         # histogram range should be offseted case by case after visualization
-        w = self.duplicate(da.from_array(self.db['v_weight']), field)
+        
+        w = self.duplicate(self.db['v_weight'], field, delayed)
         if range is None:
             range = self.get_range(field, delayed)
 
         mod = self.general_module(delayed)
-        if not delayed:
-            w = w.compute()
 
         H, edges = mod.histogram(field, bins=bins, range=range, weights=w, density=True)
         # H = H.rechunk(bins//2.5)
@@ -93,22 +96,22 @@ class calculations(database):
 
     def joint_pdf(self, field1, field2, bins=1000, ranges=[None, None], log=True, delayed=True):
         '''
+        field1 and field2 must have the same shape.
         If not delayed, fields must already be computed!
         '''
-        w = self.duplicate(da.from_array(self.db['v_weight']), field1)
+        w = self.duplicate(self.db['v_weight'], field1, delayed)
         fields = [field1, field2]
         for i, r in enumerate(ranges):
             if r is None: 
                 ranges[i] = self.get_range(fields[i], delayed)
 
         mod = self.general_module(delayed)
-        if not delayed:
-            w = w.compute()
-        
+
         # flattening the data    
         w_l = mod.ravel(w)
         f1_l = mod.ravel(field1)
         f2_l = mod.ravel(field2)
+
         H, xedges, yedges = mod.histogram2d(f1_l, f2_l, bins=bins, range=ranges, \
                             weights=w_l**2, density=True)
         H = H.T
@@ -163,10 +166,15 @@ class calculations(database):
             return C, joint[1], joint[0], [marginal[0], marginal[2]]
             # Corr - [edges] - joint_pdf - [marginal_joint_pdf, marginal_pdfs]
 
-    def duplicate(self, field, target, rechunk=False):
+    def duplicate(self, field, target, delayed=True, rechunk=False):
         # this function is useful to reshape a field properly to
         # a target array (for the purpose of dealing with same dimensions)
+        
+        # some objects in the db are not naturally dask arrays
+        if type(field) = np.ndarray and delayed=True:
+            field = da.from_array(field)
 
+        mod = self.general_module(delayed)
         # finding dimensions on which to copy initial array
         f1 = field.shape
         f2 = target.shape
@@ -175,16 +183,17 @@ class calculations(database):
             if d not in f1:
                 ax.append(i)
         ax = tuple(ax)
-        field2 = da.expand_dims(field, axis=ax)
-        if rechunk:
-            # new_chunk = tuple([1 if d not in ax else self.base_chunk[d] for d in range(4)])
-            new_chunk = field2.shape
-            field2 = field2.rechunk(new_chunk)
+        field2 = mod.expand_dims(field, axis=ax)
+
         # duplicating
         for i in ax:
-            field2 = da.repeat(field2, f2[i], axis=i)
-
-        field2 = field2.rechunk(self.base_chunk)
+            field2 = mod.repeat(field2, f2[i], axis=i)
+        if delayed:
+            if rechunk:
+                new_chunk = field2.shape
+            else:
+                new_chunk = self.base_chunk
+            field2 = field2.rechunk(new_chunk)
 
         return field2
         
