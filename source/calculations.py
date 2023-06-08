003cb790f69d94a5293122da1639d7bb8c9723ff
Nikita Allaglo <allaglo@lisn.fr>
2023-06-08 17:05:41 +0200

    Initialization of the initialization =D

--- source/calculations.py
+++ source/calculations.py
@@ -9,9 +9,12 @@ import dask.array as da
 import pickle
 
 class Calculations(Stats):
-    def __init__(self, src):
+    def __init__(self, src, tree_dir):
         '''
         This class schedules calculations regarding a source database.
-        The final results are gathered in an external.
+        The final results are gathered in an external one.
+        src: original SFEMaNS db connector path
+        tree_dir: calculations db connector path
         '''
-
+        Calculations.__init__(self, src)
+    
