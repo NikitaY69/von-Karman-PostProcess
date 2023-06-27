a7f970f546058ff03abe6c2bf636542879c8e306
Nikita Allaglo <allaglo@lisn.fr>
2023-06-27 17:37:39 +0200

    Function to compute mid points of a sampled interval

--- source/stats.py
+++ source/stats.py
@@ -308,6 +308,12 @@ class Stats(Database):
         Amax = cp.nanmax(A)
         return [Amin, Amax]
 
+    @staticmethod
+    def mid_points(edges):
+        n = len(edges)-1
+        edges = edges.get()
+        return np.array([(edges[i]+edges[i+1])/2 for i in range(n)])
+
     def key_check(self, key):
         # checks if a key is present in the db
         if key not in self.db.keys():
