945b968144f44fa626a0f454e0b606373c0e35bd
Nikita Allaglo <allaglo@lisn.fr>
2023-07-05 17:55:14 +0200

    Tutorial notebook basics complete

--- tutorials/jupyter_gpu.sh
+++ tutorials/jupyter_gpu.sh
@@ -5,8 +5,8 @@
 #SBATCH --gres=gpu:1
 #SBATCH --exclude=n51,n52,n53,n54,n55,n101,n102
 #SBATCH --mem=100GB
-#SBATCH --output=out
-#SBATCH --error=log
+#SBATCH --output=out_gpu
+#SBATCH --error=log_gpu
 
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate statistics
