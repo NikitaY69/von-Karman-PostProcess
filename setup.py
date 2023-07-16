32b789b94f10f15dab3c1fbcdbc7852677c1cd61
Nikita Allaglo <allaglo@lisn.fr>
2023-07-16 17:07:57 +0200

    More descriptive setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup, find_packages
 
-
 setup(
     name="vkpp",
     version="1.0",
+    description='Post-Processing for SFEMaNS von-Karman flow runs',
+    author='Nikita Allaglo',
+    author_email='nikita1999.allaglo@gmail.com',
     packages=find_packages(),
-)
+    )
